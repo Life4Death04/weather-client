bdd9d5e71dfe08e911fef930248b7a965d8eaead
Life4Death <santiagodrm@gmail.com>
2025-09-14 01:19:04 +0200

    feat(api): geocoding api + env file

---
+++ src/api/geocoding.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+const apiKey = import.meta.env.VITE_GEOCODE_API_KEY;
+const apiUrl = import.meta.env.VITE_GEOCODING_URL;
+
+export async function fetchGeocoding(query: string) {
+    try {
+        const rawResponse = await axios.get(`${apiUrl}?q=${query}&key=${apiKey}`);
+        const response = rawResponse.data.results[0].geometry;
+        console.log(response);
+        return response;
+    } catch (error) {
+        if(axios.isAxiosError(error)) {
+            throw new Error(error.response?.data?.message || `Error fetching geocoding data`);
+        } else {
+            throw new Error('An unexpected error occurred while fetching geocoding data');
+        }
+    }
+}
